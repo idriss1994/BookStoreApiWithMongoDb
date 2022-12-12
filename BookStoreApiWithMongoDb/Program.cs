e61275ba27b6543613177bc21afee1a1c9af2331
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-12 21:45:27 +0100

    Configuring JSON serialization options to leave property names unchanged

--- BookStoreApiWithMongoDb/Program.cs
+++ BookStoreApiWithMongoDb/Program.cs
@@ -8,7 +8,8 @@ builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
 builder.Services.AddSingleton<BooksServices>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
