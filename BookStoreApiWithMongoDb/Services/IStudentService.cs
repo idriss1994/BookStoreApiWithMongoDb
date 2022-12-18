7a5ad77459418d4e82ef2382ab94f7eab1ee8702
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-18 14:55:54 +0100

    Adding IStudentService to implement CRUD operations.

---
+++ BookStoreApiWithMongoDb/Services/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using BookStoreApiWithMongoDb.Models;
+
+namespace BookStoreApiWithMongoDb.Services
+{
+    public interface IStudentService
+    {
+        Task<List<Student>> GetAsync();
+        Task<Student> GetByIdAsync(string studentId);
+        Task<Student> CreateAsync(Student newStudent);
+        Task UpdateAsync(string studentId, Student updatedStudent);
+        Task RemoveAsync(string studentId);
+    }
+}
