cce4502e70d4f1595772ee45de81903af38b7910
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-19 21:27:01 +0100

    Implement CRUD Student operations in StudentService

---
+++ BookStoreApiWithMongoDb/Services/StudentService.cs
@@ -0,0 +1,41 @@
+﻿using BookStoreApiWithMongoDb.Models;
+using MongoDB.Driver;
+
+namespace BookStoreApiWithMongoDb.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IMongoCollection<Student> _students;
+
+        public StudentService(IStudentStoreDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _students = database.GetCollection<Student>(settings.StudentCoursesCollectionName);
+        }
+        public async Task<Student> CreateAsync(Student newStudent)
+        {
+            await _students.InsertOneAsync(newStudent);
+            return newStudent;
+        }
+
+        public async Task<List<Student>> GetAsync()
+        {
+            return await _students.Find(student => true).ToListAsync();
+        }
+
+        public async Task<Student> GetByIdAsync(string studentId)
+        {
+            return await _students.Find(student => student.Id == studentId).FirstOrDefaultAsync(); 
+        }
+
+        public async Task RemoveAsync(string studentId)
+        {
+            await _students.DeleteOneAsync(student => student.Id == studentId);
+        }
+
+        public async Task UpdateAsync(string studentId, Student updatedStudent)
+        {
+            await _students.ReplaceOneAsync(student => student.Id == studentId, updatedStudent);
+        }
+    }
+}
